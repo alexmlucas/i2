70ff369b4113387832ee1d062eeb7297649a1cd9
Alex Lucas <alexmlucas82@gmail.com>
2020-04-08 09:26:48 +0100

    Add prototypes, including new_seq

--- main_new_seq/main_new_seq.ino
+++ main_new_seq/main_new_seq.ino
@@ -2,31 +2,53 @@ const unsigned long US_IN_MINUTE = 60000000;
 const float MULTIPLIER_32ND = 0.125;
 const int PPQN = 24;
 const int PATTERN_LENGTH = 32;
+const int PATTERN_MODULO = 4;     // used when incrementing the patternCounter.
+const int QUANTISE_MODULO = 3;    // used when incrementing the quantiseCounter.
+const int NUM_OF_QUANTISE_STEPS = 64;
+const int NUM_OF_TRACKS = 8;
 
 int playFlag = true;
+int recordFlag = true;
 int midiTickInterval = 0;
 int midiTickCounter = 0;
 int patternCounter = 0;
+int quantiseCounter = 0;
 int bpm = 120;
 elapsedMicros masterClockTimer = 0;
+
+byte sequence[PATTERN_LENGTH][NUM_OF_TRACKS];
 
 void setup() {
   Serial.begin(31250);
   midiTickInterval = calculateMidiTickInterval(bpm);
   while (!Serial && millis() < 2500) /* wait for serial monitor */ ;
   Serial.println(midiTickInterval);
+
+  // intialise sequence.
+  for(int i = 0; i < PATTERN_LENGTH; i++){
+
+    for(int ii = 0; ii < NUM_OF_TRACKS; ii++){
+      sequence[i][ii] = 0;
+    }
+  }
 }
 
 void loop() {
-  if(playFlag == true) {
+  // read and store trigger events
 
-    
+  // if playing...
+  if(playFlag == true) {
+    // update the pattern counter if needed.
+    updateCounter(&patternCounter, PATTERN_MODULO, PATTERN_LENGTH);
+
+    // if recording...
+    if(recordFlag == true){
+      // add stored events to sequence if they occured previously.
+    }
+
     // check to see if the midi tick counter needs to be updated.
     if(masterClockTimer > midiTickInterval){
       updateMidiTickCounter();
-
-      // check to see if the pattern counter needs to be updated.
-      updatePatternCounter32();
 
       // reset the master clock timer
       masterClockTimer = 0;
@@ -34,18 +56,15 @@ void loop() {
   }
 }
 
-// Counts 32nd pattern step.
-void updatePatternCounter32(){
-  int pattern32Modulo = 4;  // the modulo value required to identify 32nd step events.
-
-  // if equal to zero, update value...
-  if(midiTickCounter % pattern32Modulo == 0){
-    if(patternCounter < PATTERN_LENGTH){
-      patternCounter++;
+void updateCounter(int* counter, int modulo, int maxValue){
+  if(midiTickCounter % modulo == 0){
+    //Serial.println("32nd");
+    if(*counter < maxValue){
+      (*counter)++;
     } else {
-      patternCounter = 0;
+      *counter = 0;
     }
-    Serial.println(patternCounter);    
+    Serial.println(*counter);
   }
 }
 
@@ -56,6 +75,14 @@ void updateMidiTickCounter(){
   } else {
     midiTickCounter = 0;
   }
+}
+
+// quantise trigger events. 
+int quantise(){
+  int quantiseModulo = midiTickCounter % QUANTISE_MODULO;     // There are three midi ticks per 32nd step. which one are we on?...
+  float quantiseFloat = quantiseModulo * 0.333;               // Divide the vale by three to get a float ready for rounding.
+  int quantisedStep = round(patternCounter + quantiseFloat);  // get the quantised step by adding the float to the current step, then rounding.
+  return quantisedStep;    
 }
 
 int calculateMidiTickInterval(int incomingBpm){
