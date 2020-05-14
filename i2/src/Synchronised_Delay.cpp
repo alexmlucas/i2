591fde7bf773f095c890dff7329fc25329a9fcf2
Alex Lucas <alexmlucas82@gmail.com>
2020-05-14 16:48:59 +0100

    Add Syncronised_Delay class

---
+++ i2/src/Synchronised_Delay.cpp
@@ -0,0 +1,71 @@
+#include "Synchronised_Delay.h"
+
+// ### PUBLIC ###
+
+Synchronised_Delay::Synchronised_Delay(AudioEffectDelay* delayObject)
+{
+    m_delayObject = delayObject;
+
+    for(int i = 1; i < 7; i++)                                  // iterate through the channels...
+    {
+        m_delayObject->disable(i);                              // ... to save audio memory, disable all channels other than the first.     
+    }
+}
+
+void Synchronised_Delay::assignFilterObject(AudioFilterStateVariable* filterObject)
+{
+    m_filterObject = filterObject;
+}
+
+void Synchronised_Delay::assignWetDryMixerObjects(AudioMixer4* leftWetDryMixer, AudioMixer4* rightWetDryMixer)
+{
+    m_leftWetDryMixer = leftWetDryMixer;
+    m_rightWetDryMixer = rightWetDryMixer;
+}
+
+void Synchronised_Delay::assignFeedbackAmplifierObject(AudioAmplifier* feedbackAmplifierObject)
+{
+    m_feedbackAmplifierObject = feedbackAmplifierObject;
+}
+
+void Synchronised_Delay::setDelayTime(int bpm)
+{
+    m_delayTimeMs = this->calculateTripletQuaverMs(bpm);
+    m_delayObject->delay(DELAY_TAP_CHANNEL, m_delayTimeMs);
+}
+
+void Synchronised_Delay::setEffectIntensity(float intensity)
+{
+    float wetDryMixLevel = intensity * 0.5;                     // work the intensity value to get a pleasing result
+    float feedbackLevel = intensity - 0.1;
+
+    this->setWetDryMixLevel(wetDryMixLevel);
+    this->setFeedbackLevel(feedbackLevel);
+} 
+
+// ### PRIVATE ###
+
+int Synchronised_Delay::calculateTripletQuaverMs(int bpm)
+{
+    int crotchetInterval = MS_IN_MINUTE / bpm;
+    int semiQuaverInterval = crotchetInterval / 4;
+    int tripletQuaverMs = semiQuaverInterval * 3;
+    
+    return tripletQuaverMs;
+}
+
+void Synchronised_Delay::setWetDryMixLevel(float level)
+{
+    float invertedLevel = (level - 1);                                // invert the level
+    invertedLevel = abs(invertedLevel);
+
+    m_leftWetDryMixer->gain(DRY_MIXER_CHANNEL, invertedLevel);
+    m_leftWetDryMixer->gain(WET_MIXER_CHANNEL, level);
+    m_rightWetDryMixer->gain(DRY_MIXER_CHANNEL, invertedLevel);
+    m_rightWetDryMixer->gain(WET_MIXER_CHANNEL, level);
+}
+
+void Synchronised_Delay::setFeedbackLevel(float level)
+{
+    m_feedbackAmplifierObject->gain(level);
+}
---
+++ i2/src/Synchronised_Delay.h
@@ -0,0 +1,40 @@
+#ifndef SYNCRONISED_DELAY;
+#define SYNCRONISED_DELAY;
+
+#include "Arduino.h"
+#include <Audio.h>
+
+class Synchronised_Delay
+{
+    public:
+        Synchronised_Delay(AudioEffectDelay* delayObject);
+        void assignFilterObject(AudioFilterStateVariable* filterObject);
+        void assignWetDryMixerObjects(AudioMixer4* leftWetDryMixer, AudioMixer4* rightWetDryMixer);
+        void assignFeedbackAmplifierObject(AudioAmplifier* feedbackAmplifierObject);
+        void setDelayTime(int bpm);
+        void setEffectIntensity(float intensity);
+        
+    private:
+        const int MS_IN_MINUTE = 60000;
+        const int DELAY_TAP_CHANNEL = 0;
+        const int DRY_MIXER_CHANNEL = 0;
+        const int WET_MIXER_CHANNEL = 1;
+        int m_leftMixerChannelNumber;
+        int m_rightMixerChannelNumber;
+        int m_delayTimeMs;
+        AudioEffectDelay* m_delayObject;
+        AudioFilterStateVariable* m_filterObject;
+
+        AudioMixer4* m_leftWetDryMixer;
+        AudioMixer4* m_rightWetDryMixer;
+
+        AudioAmplifier* m_feedbackAmplifierObject;
+        AudioAmplifier* m_rightFeedbackAmplifier;
+        
+        int calculateTripletQuaverMs(int bpm);
+        void setWetDryMixLevel(float level);
+        void setFeedbackLevel(float level);               // expects a value between 0 and 1.
+};
+
+
+#endif
