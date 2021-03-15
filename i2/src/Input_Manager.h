a91907127eeac249cf1a8601d87e69398904dbe9
alexmlucas <alexmlucas82@gmail.com>
2021-03-15 09:50:15 +0000

    Add rubber band fitting

--- i2/src/Input_Manager.h
+++ i2/src/Input_Manager.h
@@ -8,7 +8,7 @@ class Input_Manager
         const unsigned int MUX_READ_DELAY_US = 1;       // the delay between changing mux channel and reading it.
         const unsigned int DEBOUNCE_MS = 20;
         const int POT_NOISE_FILTER = 5;
-        const int PIEZO_THRESHOLD = 10;                 // minimum reading, avoid "noise".
+        const int PIEZO_THRESHOLD = 5;                 // minimum reading, avoid "noise".
         const unsigned int PIEZO_PEAK_MS = 2;                          // time to read peak value.
         const unsigned int PIEZO_AFTERSHOCK_MS = 10;                   // time of aftershocks and vibration.
 
